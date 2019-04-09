212477dd79acd17215e1e1d3bb0bac85e7d30a3a
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-04-09 17:41:19 -0300

    Cleaning repo and added CLI commands support

--- fast-net/fastnettool.py
+++ fast-net/fastnettool.py
@@ -1,1 +1,14 @@
-import tools
+import argparse
+
+import traceroute
+
+parser = argparse.ArgumentParser()
+parser.add_argument("address", help="Destination host address")
+parser.add_argument("-t", "--traceroute", action="store_true", dest="traceroute", default=False, help="Execute traceroute")
+parser.add_argument("-T", "--max_ttl", dest="max_ttl", type=int, default=30, help="Max TTL count")
+parser.add_argument("-c", "--count", dest="counts", type=int, default=3, help="Number of pings for each router")
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    if args.traceroute:
+        traceroute.Traceroute(args.address, max_ttl=args.max_ttl+1, counts=args.counts+1)
