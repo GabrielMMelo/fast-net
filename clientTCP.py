5fad6c7ff54da2891357b5534f3ee4431b313517
PedroPio <pedropio935@gmail.com>
2019-04-09 17:50:33 -0300

    Correção Taxa de Download

--- clientTCP.py
+++ clientTCP.py
@@ -11,22 +11,29 @@ s = socket.socket(socket.AF_INET,
 				socket.SOCK_STREAM)
 s.connect((tcp_ip, tcp_port))
 #conn, addr = s.accept()
-def calc_upload():
-	arquivo =  open('testando.txt','rb')
-
-	pedaco_arquivo = arquivo.read(1024)
-	while(pedaco_arquivo):
-		s.send (pedaco_arquivo)
-		pedaco_arquivo = arquivo.read(1024)
-	tempo_final = time.time()
-	arquivo.close()
-	tamanho_arquivo_bits = (os.stat('testando.txt').st_size)/131072
-	return tamanho_arquivo/ (tempo_inicial-tempo_final)
-
+# def calc_upload():
+# 	arquivo =  open('testando.txt','rb')
+# 	print("entrei")
+# 	tempo_inicial = time.time()
+# 	print("Contando o tempo")
+# 	pedaco_arquivo = arquivo.read(1024)
+# 	print("li pedaco arquivo")
+# 	while(pedaco_arquivo):
+# 		s.send(pedaco_arquivo)
+# 		print(pedaco_arquivo.decode())
+# 		pedaco_arquivo = arquivo.read(1024)
+# 	print("Sai do while")
+# 	tempo_final = time.time()
+# 	conn.send(str.encode("Ok"))
+	
+# 	arquivo.close()	
+# 	tamanho_arquivo_bits = (os.path.getsize('testando.txt'))/(1024*1024*8)
+# 	("Velocidade de Upload: " , tamanho_arquivo_bits/ (tempo_final-tempo_inicial))
 
 def download():
-	tempo_inicial = time.time()
 	s.send(str.encode('requisitando'))
+	arquivo = open('testando.txt','rb')
+	tempo_inicial = time.time()
 	pedaco_arquivo = s.recv(1024)
 	while(pedaco_arquivo):
 		#arquivo.write(pedaco_arquivo)
@@ -35,16 +42,25 @@ def download():
 			print (pedaco_arquivo.decode())
 			break
 	tempo_final = time.time()
-	#arquivo.close()
-	tamanho_arquivo_bits = (os.path.getsize('testando.txt'))/(1024*1024*8)
-	print ("Velocidade de download :",  tamanho_arquivo_bits/ (tempo_final-tempo_inicial), "Mbs")
+	arquivo.close()
+	#print(tempo_inicial)
+	#print(tempo_final)
+	
+	tamanho_arquivo_bits = (os.path.getsize('testando.txt'))/(1024*1024)
+	#print(os.path.getsize('testando.txt'))
+	#print(tamanho_arquivo_bits)
+	print (tempo_final-tempo_inicial)
+	print ("Velocidade de download :",  tamanho_arquivo_bits/(tempo_final-tempo_inicial), "Mbs")
 	
 
 
 while True:
+	
 	download()
+
+	#calc_upload()
+
 	data = s.recv(1024)
-	calc_upload()
 	if not data:
 		break
 	
