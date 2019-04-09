0a4616deb7c587846c515903a96e066d065c9298
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-04-09 17:52:23 -0300

    Fixed cli commands

--- fast-net/fastnettool.py
+++ fast-net/fastnettool.py
@@ -4,8 +4,8 @@ import traceroute
 
 parser = argparse.ArgumentParser()
 parser.add_argument("address", help="Destination host address")
-parser.add_argument("-t", "--traceroute", action="store_true", dest="traceroute", default=False, help="Execute traceroute")
-parser.add_argument("-T", "--max_ttl", dest="max_ttl", type=int, default=30, help="Max TTL count")
+parser.add_argument("-t", "--traceroute", action="store_true", dest="traceroute", default=False, help="Execute traceroute tool for `address`")
+parser.add_argument("-T", "--max_ttl", dest="max_ttl", type=int, default=30, help="Max TTL counter")
 parser.add_argument("-c", "--count", dest="counts", type=int, default=3, help="Number of pings for each router")
 args = parser.parse_args()
 
